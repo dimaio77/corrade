0c3de9ccba6328f66b8724c43391db2034f4bf72
Cari Tedeschi <cari.tedeschi@gmail.com>
2018-11-11 21:39:01 +0000

    Removed the ports

--- host/start.sh
+++ host/start.sh
@@ -6,8 +6,8 @@ WORKING_DIR=/path/to/working/directory
 # Change this to the name of the container that you have based on carissima/corrade
 CONTAINER=YOUR_CONTAINER_NAME_HERE
 
-sudo docker run -d --rm -v ${WORKING_DIR}/Cache\:/corrade/Cache -v ${WORKING_DIR}/Databases\:/corrade/Databases\
--v ${WORKING_DIR}/State\:/corrade/State -v ${WORKING_DIR}/Logs\:/corrade/Logs \
--P -p 53\:53 -p 80\:80 -p 443\:443 -p 12043\:12043 -p 12046\:12046 -p 21002\:21002 \
--p 53\:53/udp -p 3478\:3478/udp -p 3479\:3479/udp -p 5060\:5060/udp -p 5062\:5062/udp \
---name corrade ${CONTAINER} /usr/bin/mono-service -l\:Corrade.exe.lock --no-daemon Corrade.exe
+sudo docker run -d --rm -v ${WORKING_DIR}/Cache\:/corrade/Cache \
+    -v ${WORKING_DIR}/Databases\:/corrade/Databases \
+    -v ${WORKING_DIR}/State\:/corrade/State \
+    -v ${WORKING_DIR}/Logs\:/corrade/Logs \
+    --name corrade ${CONTAINER} /usr/bin/mono-service -l\:Corrade.exe.lock --no-daemon Corrade.exe
